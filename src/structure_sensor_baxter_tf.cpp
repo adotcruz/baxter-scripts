4ee6c6312f770444c8ee9eda5b4d56d0f0c3fd25
Anthony <anthony_cruz@brown.edu>
2018-03-14 18:54:18 -0400

    Initial structure sensor static TF and launch file

---
+++ src/structure_sensor_baxter_tf.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <cstdio>
+#include <tf2/LinearMath/Quaternion.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc,argv, "structure_sensor_baxter_static_tf_broadcaster");
+
+  static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+  geometry_msgs::TransformStamped static_transformStamped;
+
+  static_transformStamped.header.stamp = ros::Time::now();
+  // TODO: Parameterize right and left
+  static_transformStamped.header.frame_id = "right_gripper_base";
+  static_transformStamped.child_frame_id = "structure_sensor_link";
+  static_transformStamped.transform.translation.x = 0;
+  static_transformStamped.transform.translation.y = 0;
+  static_transformStamped.transform.translation.z = 0;
+  tf2::Quaternion quat;
+  quat.setRPY(0, 0, 0);
+  static_transformStamped.transform.rotation.x = quat.x();
+  static_transformStamped.transform.rotation.y = quat.y();
+  static_transformStamped.transform.rotation.z = quat.z();
+  static_transformStamped.transform.rotation.w = quat.w();
+  static_broadcaster.sendTransform(static_transformStamped);
+
+  ros::spin();
+  return 0;
+};
